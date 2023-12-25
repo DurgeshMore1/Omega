7e3e6d9408f9987ffe5712250c431534cbfb7f1f
Durgesh <durgeshmore809@gmail.com>
2023-12-25 07:12:26 +0530

    pom file changes

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Student {
+    int rollno=20;
+    String name ="wewq";
+    public void studentDetails()
+    {
+        System.out.println("roll no"+rollno);
+        System.out.println("student name"+name);
+    }
+}
